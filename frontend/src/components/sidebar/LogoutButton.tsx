37295156cb663ed389c2f54735cc05d2b75bbd2a
Vivek kumar <vivek.buisness@gmail.com>
2025-03-17 23:46:31 +0530

    signup login and logout functionalities added

--- frontend/src/components/sidebar/LogoutButton.tsx
+++ frontend/src/components/sidebar/LogoutButton.tsx
@@ -1,10 +1,8 @@
 import { LogOut } from "lucide-react";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
-  const logout = () => {
-    alert("You are logged out");
-  };
-
+  const { logout } = useLogout();
   return (
     <div className="mt-auto">
       <LogOut className="w-6 h-6 text-white cursor-pointer" onClick={logout} />
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -7,6 +7,7 @@ import {
   useEffect,
   useState,
 } from "react";
+import toast from "react-hot-toast";
 
 type AuthUserType = {
   id: string;
@@ -45,6 +46,7 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
         setAuthUser(data);
       } catch (error: any) {
         console.error(error);
+        toast.error(error.message);
       } finally {
         setIsLoading(false);
       }
